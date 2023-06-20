5b3634ce2b77657d32f894cdd6b326ae498affd6
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-06-20 19:59:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const { Client } = require('replit-storage');
+const Database = require('@replit/database');
+const cors = require('cors');
+
+const client = new Client();
+const db = new Database(process.env.REPLIT_DB_URL); // Pass the REPLIT_DB_URL to the Database constructor
+const app = express();
+
+const { WORDNIK_API_KEY } = process.env;
+
+app.use(cors()); // Enable CORS for all routes
+
+app.use(express.json()); // Parse JSON request bodies
+
+// Add a leaderboard entry
+app.post('/leaderboard', async (req, res) => {
+  try {
+    const { name, score, time } = req.body;
+
+    // Validate the request data
+    if (!name || !score || !time) {
+      return res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    // Create a unique key for the entry
+    const entryId = generateEntryId();
+
+    // Store the entry in the database
+    await db.set(entryId, { name, score, time });
+
+    return res.status(201).json({ message: 'Leaderboard entry added successfully' });
+  } catch (error) {
+    console.error('Error adding leaderboard entry:', error);
+    return res.status(500).json({ message: 'Failed to add leaderboard entry' });
+  }
+});
+
+// Get leaderboard entries
+app.get('/leaderboard', async (req, res) => {
+  try {
+    // Get all keys in the database
+    const keys = await db.list();
+
+    // Retrieve the entries for each key
+    const entries = await Promise.all(keys.map(async (key) => {
+      const entry = await db.get(key);
+      return entry;
+    }));
+
+    // Sort the entries by score in descending order
+    const sortedEntries = entries.sort((a, b) => b.score - a.score);
+
+    return res.status(200).json(sortedEntries);
+  } catch (error) {
+    console.error('Error retrieving leaderboard entries:', error);
+    return res.status(500).json({ message: 'Failed to retrieve leaderboard entries' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// Generate a unique entry ID
+function generateEntryId() {
+  return Date.now().toString();
+}
